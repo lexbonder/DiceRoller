c183ddaa6f523ab6ea8025bf64cca348964f833d
Alex Bonder <lexbonder@gmail.com>
2023-10-15 13:08:24 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Alex Bonder on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
